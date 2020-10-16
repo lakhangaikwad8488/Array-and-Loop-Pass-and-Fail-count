e5bf1eea002fc2e8770855b16610d58556cdf7d5
lakhangaikwad8488 <70312128+lakhangaikwad8488@users.noreply.github.com>
2020-10-16 12:01:06 +0530

    Add files via upload

---
+++ src/main/java/main/java/yaksha/Count.java
@@ -0,0 +1,11 @@
+package main.java.yaksha;
+
+import java.util.Scanner;
+
+public class Count {
+	public int isPass(int n,int arr[]) {
+ 		 
+		/*You code here*/
+		return 0;
+	}
+}
---
+++ src/main/java/main/java/yaksha/MainCount.java
@@ -0,0 +1,22 @@
+package main.java.yaksha;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainCount {
+	 public static void main (String[] args) throws IOException {
+	       Scanner sc=new Scanner(System.in);
+	       Count c=new Count();
+	        System.out.println("Enter the no of subjects:");
+	        int n=sc.nextInt();
+	        int count=0;
+	        if(n<=0)
+	        {
+	           /*Your code here*/
+	        }
+	        else
+	        {
+	        	  /*Your code here*/
+	        }
+	    }
+}
